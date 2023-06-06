78756f9f0efe5e860977994ee5a9b2d162053035
shimahi <shimahi@reah.jp>
2023-06-07 02:10:39 +0900

    fix

--- packages/app/app/routes/_index.tsx
+++ packages/app/app/routes/_index.tsx
@@ -11,7 +11,6 @@ export const meta: V2_MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix</h1>
       <h2>{message}</h2>
     </div>
   )
